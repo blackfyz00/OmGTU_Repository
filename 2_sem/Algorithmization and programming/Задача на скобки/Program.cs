600e50c69362a2b9b2e8d3f83ebbf0b7eb11a919
blackfyz00 <159511274+blackfyz00@users.noreply.github.com>
2024-05-24 10:38:32 +0600

    new

---
+++ 2_sem/Algorithmization and programming/Задача на скобки/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        bool answ = false;
+        Stack<string> stack = new Stack<string>();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == '(') { stack.Push("("); }
+            if (str[i] == '[') { stack.Push("["); }
+            if (str[i] == '{') { stack.Push("{"); }
+
+            if (str[i] == ')')
+            {
+                if (Are_Stack_Find(stack, '(') == true) answ = true;
+                else { answ = false; break; }
+            }
+            if (str[i] == ']')
+            {
+                if (Are_Stack_Find(stack, '[') == true) answ = true;
+                else { answ = false; break; }
+            }
+            if (str[i] == '}')
+            {
+                if (Are_Stack_Find(stack, '{') == true) answ = true;
+                else { answ = false; break; }
+            }
+        }
+
+        if (answ == true)
+            Console.WriteLine("Скобки расставлены правильно");
+        else
+            Console.WriteLine("Скобки расставлены неправильно");
+    }
+
+    static bool Are_Stack_Find(Stack<string> stack, char s)
+    {
+        if (stack.Count == 0)
+            return false;
+        else
+        {
+            if (stack.Pop() == Convert.ToString(s))
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ 2_sem/Algorithmization and programming/Обратная польская запись/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string polish = Console.ReadLine(); string[] entrypolish = new string[polish.Length];
+        for (int i = 0; i < polish.Length; i++)
+            entrypolish[i]= Convert.ToString(polish[i]);
+
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < entrypolish.Length; i++)
+        {
+            if (entrypolish[i] != "+" && entrypolish[i] != "-" && entrypolish[i] != "*" && entrypolish[i] != "/")
+                stack.Push(Convert.ToInt32(entrypolish[i]));
+
+            else
+            {
+                switch (entrypolish[i])
+                {
+                    case "+":
+                        if (stack.Count > 1)
+                        {
+                            int n1 = stack.Pop();
+                            int n2 = stack.Pop();
+                            stack.Push(n1 + n2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: нехватка или избыток элементов для выполнения действия");
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case "-":
+                        if (stack.Count > 1)
+                        {
+                            int n3 = stack.Pop();
+                            int n4 = stack.Pop();
+                            stack.Push(n4 - n3);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: нехватка или избыток элементов для выполнения действия");
+                            Environment.Exit(0);
+                        }
+
+                        break;
+                    case "*":
+                        if (stack.Count > 1)
+                        {
+                            int n5 = stack.Pop();
+                            int n6 = stack.Pop();
+                            stack.Push(n5 * n6);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: нехватка или избыток элементов для выполнения действия");
+                            Environment.Exit(0);
+                        }
+                        break;
+                    case "/":
+                        if (stack.Count > 1)
+                        {
+                            int n7 = stack.Pop();
+                            int n8 = stack.Pop();
+                            if (n7 != 0)
+                                stack.Push(n8 / n7);
+                            else
+                            {
+                                Console.WriteLine("Невозможно поделить на ноль");
+                                Environment.Exit(0);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: нехватка или избыток элементов для выполнения действия");
+                            Environment.Exit(0);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("знак действия не распознан, введите корректный пример");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+        }
+        if (stack.Count > 1)
+            Console.WriteLine("Ошибка: нехватка действия");
+        else
+            Console.WriteLine($"Результат: {Convert.ToInt32(stack.Peek())}");
+
+    }
+}
